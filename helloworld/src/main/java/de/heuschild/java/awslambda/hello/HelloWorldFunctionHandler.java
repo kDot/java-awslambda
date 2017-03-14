2dd26570cbd1b73f705457e6539cd6186100b30c
k. <k-laus@gmx.net>
2017-03-14 21:25:32 +0100

    Update with log and shade-plugin

--- helloworld/src/main/java/de/heuschild/java/awslambda/hello/HelloWorldFunctionHandler.java
+++ helloworld/src/main/java/de/heuschild/java/awslambda/hello/HelloWorldFunctionHandler.java
@@ -7,6 +7,11 @@ public class HelloWorldFunctionHandler implements RequestHandler<String, String>
 
 	public String handleRequest(final String input, final Context context) {
 		context.getLogger().log("kMethod called");
+		System.out.println("Function name: " + context.getFunctionName());
+		System.out.println("Max mem allocated: " + context.getMemoryLimitInMB());
+		System.out.println("Time remaining in milliseconds: " + context.getRemainingTimeInMillis());
+		System.out.println("CloudWatch log stream name: " + context.getLogStreamName());
+		System.out.println("CloudWatch log group name: " + context.getLogGroupName());
 		return "Hello " + input + "!";
 	}
 
