0d1562841a70327d4b12c40bf5517f478064c2f8
k. <k-laus@gmx.net>
2017-03-14 21:46:20 +0100

    add another k

--- helloworld/src/main/java/de/heuschild/java/awslambda/hello/HelloWorldFunctionHandler.java
+++ helloworld/src/main/java/de/heuschild/java/awslambda/hello/HelloWorldFunctionHandler.java
@@ -6,7 +6,7 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 public class HelloWorldFunctionHandler implements RequestHandler<String, String> {
 
 	public String handleRequest(final String input, final Context context) {
-		context.getLogger().log("kMethod called");
+		context.getLogger().log("kkMethod called");
 		System.out.println("Function name: " + context.getFunctionName());
 		System.out.println("Max mem allocated: " + context.getMemoryLimitInMB());
 		System.out.println("Time remaining in milliseconds: " + context.getRemainingTimeInMillis());
