a70520ea24dde2fc870d917270d5ba79687028ff
k <k-laus@gmx.net>
2017-12-23 22:36:22 +0100

    Refactor structure

--- helloworld/src/main/java/de/heuschild/java/awslambda/hello/HelloWorldFunctionHandler.java
+++ helloworld/src/main/java/de/heuschild/java/awslambda/hello/HelloWorldFunctionHandler.java
@@ -1,5 +1,11 @@
 package de.heuschild.java.awslambda.hello;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
@@ -12,6 +18,12 @@ public class HelloWorldFunctionHandler implements RequestHandler<String, String>
 		System.out.println("Time remaining in milliseconds: " + context.getRemainingTimeInMillis());
 		System.out.println("CloudWatch log stream name: " + context.getLogStreamName());
 		System.out.println("CloudWatch log group name: " + context.getLogGroupName());
+
+		final AmazonDynamoDB client = AmazonDynamoDBClientBuilder.defaultClient();
+		final Map<String, AttributeValue> valueMap = new HashMap<String, AttributeValue>();
+		valueMap.put("value", new AttributeValue("Created on "));
+		client.putItem("T_Entry", valueMap);
+
 		return "Hello " + input + "!";
 	}
 
